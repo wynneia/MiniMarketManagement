e9af2b1c7a9e96c805ea9d6d0a8a3e78e1bb9284
wynneia <kakakcantqq@gmail.com>
2025-04-16 23:02:43 +0000

    Fix login endpoint route to use '/login' instead of '/auth'

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,7 @@ router.post('/register', async (req, res) => {
 });
 
 // Login endpoint
-router.post('/auth', async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
